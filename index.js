60abf03f3a8db2ec746e7de6bd8a9bb761ca5fa5
Veronica Bustabad <vbustabad@gmail.com>
2018-09-08 01:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function (drivers) { return drivers.slice(0, 2) };
+
+const returnLastTwoDrivers = function (drivers) { return drivers.slice((drivers.length - 1), (drivers.length - 2))};
