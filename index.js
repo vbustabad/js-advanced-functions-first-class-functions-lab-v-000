fac974b7d4f7341483bf266839b55bc5099511e7
Veronica Bustabad <vbustabad@gmail.com>
2018-09-08 01:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@
 const returnFirstTwoDrivers = function (drivers) { return drivers.slice(0, 2) };
 
 const returnLastTwoDrivers = function (drivers) { return drivers.slice(-2)};
+
+selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
