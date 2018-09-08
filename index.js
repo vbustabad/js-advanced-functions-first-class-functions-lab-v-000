4623939eacaabb46c8d423f68ccee3b052887b6b
Veronica Bustabad <vbustabad@gmail.com>
2018-09-08 01:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Code your solution in this file!
 
-const new_drivers_array = (function returnFirstTwoDrivers(drivers) {})();
+const new_drivers_array = (function returnFirstTwoDrivers(drivers) {
+  return drivers.splice(0, 2);
+}
+})();
